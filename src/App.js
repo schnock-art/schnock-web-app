efa2b25ecd5b7238a5d58cc20031a7ad6f7e8b59
Jan <schnock.art@gmail.com>
2023-12-04 17:45:02 -0300

    A bit prettier with modal image on click

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, {useState, useEffect} from 'react';
-//import { Storage } from 'aws-amplify';
-import { StorageManager,StorageImage } from '@aws-amplify/ui-react-storage';
-//import {StorageManagerFileTypesExample} from './components/FileUploader';
-import { Collection, ThemeProvider } from '@aws-amplify/ui-react';
-//import {S3ProviderListOutpuItem} from "aws-amplify/storage"
+import { StorageManager, StorageImage } from '@aws-amplify/ui-react-storage';
+import { ThemeProvider } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css'
 import './App.css';
 import { list } from 'aws-amplify/storage';
-
-
-
+import ImageModal from './components/ImageModal';
 
 const globalTheme = {
   name: 'my-theme',
@@ -27,62 +22,92 @@ const globalTheme = {
   },
 };
 
-interface FileKey {
-  file: File;
-  key: string;
-}
-
 
 const App = () =>{
-  const [photos, setPhotos] = useState([]);
+  const [images, setPhotos] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   
+  const handleImageSelect = (imgKey) => {
+      setSelectedImage(imgKey);
+  };
+
+  const handleCloseModal = () => {
+      setSelectedImage(null);
+  };
+    
 
   const processFile = async ({ file }) => {
-    const fileExtension = file.name.split('.').pop();
-  
+    // Extract the path and filename
+    const pathArray = "photos".split('/');
+    const fileName = file.name;
+    const path = pathArray.join('/') + (pathArray.length > 0 ? '/' : '');
+    const fileExtension = fileName.split('.').pop();
+    console.log('File:', file);
+    console.log('patharray', pathArray)
+    console.log('File Extension:', fileExtension);
+    console.log('File Name:', fileName);
+    console.log('Path:', path);
+   
+
     return file
       .arrayBuffer()
-      .then((filebuffer) => window.crypto.subtle.digest('SHA-1', filebuffer))
-      .then((hashBuffer) => {
+      .then(filebuffer => window.crypto.subtle.digest('SHA-1', filebuffer))
+      .then(hashBuffer => {
         const hashArray = Array.from(new Uint8Array(hashBuffer));
-        const hashHex = hashArray
-          .map((a) => a.toString(16).padStart(2, '0'))
-          .join('');
-        return { file, key: `${hashHex}.${fileExtension}` ,
-        metadata: {
-          id: `${hashHex}.${fileExtension}`,
-        },};
+        const hashHex = hashArray.map(a => a.toString(16).padStart(2, '0')).join('');
+        
+        // Construct the key with the path and hashed filename
+        const hashedFileName = `${hashHex}.${fileExtension}`;
+        const key = `${path}${hashedFileName}`;
+
+        return {
+          file,
+          key,
+          metadata: {
+            id: key,
+            name: fileName, // Original file name
+          },
+        };
       });
   };
 
-  useEffect(() => {
-    async function fetchPhotos() {
-      try {
-        // Fetch all files from the 'photos/' directory
-        const response = await list({ 
-          prefix: '',
-          postfix: '.jpg',
-          options: { 
-            accessLevel: 'public', 
-            listAll: true 
-          } 
-        });
-        console.log('Listed Items:', response.items);
-        // Filter and process the response to get photo URLs
-        setPhotos(response.items.map(file => file.key)); // Assuming file has a URL attribute
-        console.log('Photos:', photos);
-      } catch (error) {
-        console.error('Error fetching photos', error);
-      }
-    }
+  const fetchPhotos = async() => {
+    try {
+      // Fetch all files from the 'photos/' directory
+      const response = await list({ 
+        prefix: 'photos/',
+        postfix: '.jpg',
+        options: { 
+          accessLevel: 'public', 
+          listAll: true 
+        } 
+      });
 
-    fetchPhotos();
+      
+
+      console.log('Listed Items:', response.items);
+
+      const validPhotos = response.items
+      .filter(item => item.key && item.key.trim() !== 'photos/') // Filter out empty or whitespace-only keys
+
+
+      // Filter and process the response to get photo URLs
+      setPhotos(validPhotos.map(file => file.key)); // Assuming file has a URL attribute
+      console.log('Photos:', validPhotos);
+    } catch (error) {
+      console.error('Error fetching photos', error);
+    }
+  };
+
+  // Fetch and set images from storage on component mount
+  useEffect(() => {
+      fetchPhotos();
   }, []);
 
   useEffect(() => {
-    console.log('Updated Photos:', photos);
-  }, [photos]);
+    console.log('Updated Photos:', images);
+  }, [images]);
 
   return (
     <ThemeProvider theme={globalTheme}>
@@ -104,20 +129,26 @@ const App = () =>{
       maxFileSize={1000000}
       processFile={processFile}
       path="photos/"
-      //onUploadSuccess={onSucess}
+      onUploadSuccess={fetchPhotos}
+      CacheControl="max-age=86400"
     />
+    <div><h1>Photo Gallery</h1></div>
     <div className="photo-gallery">
-      <h1>Photo Gallery</h1>
-      {photos.map(photo => (
+      {images.map(photo => (
         <StorageImage
-          //alt={photo.key}
-          //key={photo.key}
+          alt={photo}
+          key={photo}
           imgKey={photo}
           accessLevel="public"
-          style={{ width: '100%', height: '200px' }}
+          style={{ width: '100%', height: '100%' }} // This ensures the image takes the full size of its container
+          onStorageGetError={(error) => console.error(error)}
+          onClick={() => handleImageSelect(photo)} // Add click handler
         />
       ))}
     </div>
+    {selectedImage && (
+      <ImageModal imgKey={selectedImage} onClose={handleCloseModal} />
+    )}
     </ThemeProvider>
   )
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,18 @@
-import {Grid} from "@aws-amplify/ui-react";
+import { Grid } from "@aws-amplify/ui-react";
 import React from "react";
 
-export function Layout(
-    {children}: { children: React.ReactNode }
-) {
+// Layout component using AWS Amplify's Grid.
+// Props:
+// - children: React nodes to be rendered inside the grid.
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+export function Layout({ children }: LayoutProps) {
     return (
         <Grid
-            className="Schnock"
+            className="main-grid-layout" // Renamed class for clarity
             gap="1rem"
             templateRows="auto 1fr"
             height="100%"
